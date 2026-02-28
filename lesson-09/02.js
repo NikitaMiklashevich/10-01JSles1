54dde6f2422accebc4570f9dabfe4fcab713fe9e
NikitaMiklashevich <nikmik.nikita@gmail.com>
2026-02-28 22:50:50 +0300

    28/2

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -39,7 +39,7 @@ let isTimerStarted = false
 let timerId
 
 startButton.addEventListener('click', () => {
-    
+    if(!isTimerStarted){
     let counter = 3
     countdownDisplay.innerHTML= counter
     timerId = setInterval(function print() {
@@ -52,15 +52,17 @@ startButton.addEventListener('click', () => {
         countdownDisplay.innerHTML= ' '
         clearInterval(timerId);
         countdownDisplay.append('🚀');
-        
+        isTimerStarted = false
     }
 }, 1000)
 
-    // your code
+}    // your code
 })
 
 cancelButton.addEventListener('click', () => {
     // your code
+    if(isTimerStarted = true)
     clearInterval(timerId);
     countdownDisplay.innerHTML= 'Отменено'
+    isTimerStarted = false
 })
